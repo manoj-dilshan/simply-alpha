f0f49a686003c990c4157535b40c44de1a4ddeb1
Manoj Dilshan <krmdp@outlook.com>
2025-06-10 21:09:59 +0530

    update packeges and  add navbar

---
+++ app/(home)/page.tsx
@@ -0,0 +1,10 @@
+import Navbar10 from "@/components/home/navbar";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar10/>
+    </div>
+  );
+}
