85984a0cb1aec703ff46096f5d58e88827e04726
Manoj Dilshan <krmdp@outlook.com>
2025-11-23 13:24:03 +0530

    Update backend

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,9 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import path from "path";
 
-import { connectDB } from "./db/connectDB.js";
+import { connectDB } from "./src/config/db.js";
 
-import authRoutes from "./routes/auth.route.js";
+import authRoutes from "./src/routes/auth.route.js";
 
 dotenv.config();
 
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 app.use(express.json()); // allows us to parse incoming requests:req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
@@ -22,14 +22,21 @@ app.use(cookieParser()); // allows us to parse incoming cookies
 app.use("/api/auth", authRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.use(express.static(path.join(__dirname, "frontend/.next")));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+		res.sendFile(path.resolve(__dirname, "frontend/.next", "server", "pages", "index.html"));
 	});
 }
 
-app.listen(PORT, () => {
-	connectDB();
-	console.log("Server is running on port: ", PORT);
-});
+const startServer = async () => {
+	try {
+		await connectDB();
+		app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+	} catch (err) {
+		console.error("Failed to start server:", err);
+		process.exit(1);
+	}
+};
+
+startServer();
